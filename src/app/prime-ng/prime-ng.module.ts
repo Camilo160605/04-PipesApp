5eb7823a3438d61339e8b9fb51bdeccf592de746
Camilo160605 <camilo1665ober@gmail.com>
2023-11-09 16:21:58 -0500

    first things han been changed

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { MenuModule } from 'primeng/menu';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MenuModule
+  ]
+})
+export class PrimeNgModule { }
