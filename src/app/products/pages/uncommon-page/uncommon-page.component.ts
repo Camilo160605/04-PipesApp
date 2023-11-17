46e17fee78344f5a668f0f18d3035f8fcaea0d68
Camilo160605 <camilo1665ober@gmail.com>
2023-11-17 17:29:34 -0500

    uncommon page has been created

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+    public name : string ="Camilo";
+    public gender : 'male' | 'female' = 'male';
+    public invitationMap = {
+      male : 'invitarlo',
+      female : 'invitarla'
+    }
+    changeName(): void {
+      this.name = 'Melissa',
+      this.gender = 'female'
+    }
+
+  //i18nPlural Pipe
+  public clients : string[] = ['Camilo','Eduardo','Maria','Jorge','Paola','Diego','Mateo','Juan','Pedro','Julian'];
+  public clientsMap = {
+     '=0' : 'No tenemos ningun cliente esperando.',
+     '=1': 'Tenemos un cliente esperando.',
+     '=2': 'Tenemos 2 esperando.',
+     'other' : 'tenemos # clientes esperando.',
+  }
+
+  deleteClient():void{
+    this.clients.shift()
+  }
 }
