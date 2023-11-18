19020e4cc7b2abf2bb0f7f8e84b86f36c9a1f144
Camilo160605 <camilo1665ober@gmail.com>
2023-11-18 11:45:20 -0500

    some things has been changed by sortline ascending

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 // import { MenuModule } from 'primeng/menu';
-import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button'
 import { CardModule } from 'primeng/card'
 import { FieldsetModule } from 'primeng/fieldset'
+import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel'
+import { ToolbarModule } from 'primeng/toolbar';
 
 
 @NgModule({
   exports: [
     // MenuModule,
-    MenubarModule,
     ButtonModule,
     CardModule,
     FieldsetModule,
-    PanelModule
+    MenubarModule,
+    PanelModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }  
